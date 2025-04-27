78eb8fbc8f7db010c0a9bdd31be9d4a3484535fd
sagark8s <157510953+sagark8s@users.noreply.github.com>
2025-04-28 03:34:25 +0530

    Update 07-aks-cluster.tf

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -3,14 +3,14 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   location            = azurerm_resource_group.aks_rg.location
   name                = "${azurerm_resource_group.aks_rg.name}-cluster"
   resource_group_name = azurerm_resource_group.aks_rg.name
-  kubernetes_version  = 1.31
+  kubernetes_version  = "2023-01-01"
   #kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
   node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
 
   default_node_pool {
     name       = "systempool"
     vm_size    = "Standard_DS2_v2"
-    #orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
+    orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
     availability_zones   = [1, 2, 3]
     enable_auto_scaling  = true
     max_count            = 3
