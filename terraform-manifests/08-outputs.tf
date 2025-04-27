ee30d1e8cfca5ec6ed0402b297484aaca6aacfc8
sagark8s <157510953+sagark8s@users.noreply.github.com>
2025-04-28 03:05:56 +0530

    Update 08-outputs.tf

--- terraform-manifests/08-outputs.tf
+++ terraform-manifests/08-outputs.tf
@@ -16,24 +16,6 @@ output "resource_group_name" {
   value = azurerm_resource_group.aks_rg.name
 }
 
-# Azure AKS Versions Datasource
-output "versions" {
-  value = data.azurerm_kubernetes_service_versions.current.versions
-}
-
-output "latest_version" {
-  value = data.azurerm_kubernetes_service_versions.current.latest_version
-}
-
-# # Azure AD Group Object Id
-# output "azure_ad_group_id" {
-#   value = azuread_group.aks_administrators.id
-# }
-# output "azure_ad_group_objectid" {
-#   value = azuread_group.aks_administrators.object_id
-# }
-
-
 # Azure AKS Outputs
 
 output "aks_cluster_id" {
@@ -44,6 +26,23 @@ output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks_cluster.name
 }
 
-output "aks_cluster_kubernetes_version" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
-}
+# Azure AKS Versions Datasource
+#output "versions" {
+  #value = data.azurerm_kubernetes_service_versions.current.versions
+#}
+
+#output "latest_version" {
+  #value = data.azurerm_kubernetes_service_versions.current.latest_version
+#}
+
+#output "aks_cluster_kubernetes_version" {
+  #value = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
+#}
+
+# # Azure AD Group Object Id
+# output "azure_ad_group_id" {
+#   value = azuread_group.aks_administrators.id
+# }
+# output "azure_ad_group_objectid" {
+#   value = azuread_group.aks_administrators.object_id
+# }
